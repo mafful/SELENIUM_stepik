90595fa711f9a5bd4ef251d136d71202dcf510e9
Vasya Vasin <vasya@example.com>
2024-12-19 05:57:06 +0300

    combine tests into one file

--- Chapter_4/test_product_page.py
+++ Chapter_4/test_product_page.py
@@ -133,3 +133,50 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.should_not_be_basket_items()
     logger.info("Basket is empty!")
     time_to_see(5)
+
+
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, product_link):
+    page = ProductPage(
+        browser, product_link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+
+    logger.info(f"Opening main page {product_link}...")
+    page.open()
+    logger.info("Main page opened successfully.")
+
+    page.add_to_basket()
+    logger.info("Add to basket button clicked.")
+
+    page.should_not_be_success_message()
+    logger.info("Success message is NOT presented")
+
+
+def test_guest_cant_see_success_message(browser, product_link):
+    page = ProductPage(
+        browser, product_link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+
+    logger.info(f"Opening main page {product_link}...")
+    page.open()
+    logger.info("Main page opened successfully.")
+
+    page.should_not_be_success_message()
+    logger.info("Success message is NOT presented")
+
+
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_message_disappeared_after_adding_product_to_basket(browser, product_link):
+    page = ProductPage(
+        browser, product_link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+
+    logger.info(f"Opening main page {product_link}...")
+    page.open()
+    logger.info("Main page opened successfully.")
+
+    page.add_to_basket()
+    logger.info("Add to basket button clicked.")
+
+    page.should_disappear_within_certain_time()
+    logger.info("Success message has disappeared")
